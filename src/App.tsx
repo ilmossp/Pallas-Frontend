4a36a384d47e8ea7b6ad6b9a8cdb4a6f2dba9134
ilmossp <ilyamodeni@gmail.com>
2023-10-21 23:54:10 +0100

    feat:added Table

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from "./components/Layout";
 import { getQueue } from "./lib/api";
 import PieChart from "./components/PieChart";
 import Stats from "./components/Stats";
+import Table from "./components/Table";
 
 const App = () => {
   const { data, isSuccess } = useQuery("getQueue", getQueue, { retry: true });
@@ -14,14 +15,21 @@ const App = () => {
           <span className="loading loading-infinity loading-lg"></span>
         )}
         {isSuccess && (
-          <div className="flex gap-5">
-            <PieChart queue={data} title="Protocols" element="proto"></PieChart>
-            <PieChart
-              queue={data}
-              title="Services"
-              element="service"
-            ></PieChart>
-            <Stats queue={data}></Stats>
+          <div className="space-y-5">
+            <div className="flex gap-5">
+              <PieChart
+                queue={data}
+                title="Protocols"
+                element="proto"
+              ></PieChart>
+              <PieChart
+                queue={data}
+                title="Services"
+                element="service"
+              ></PieChart>
+              <Stats queue={data}></Stats>
+            </div>
+            <Table queue={data}></Table>
           </div>
         )}
       </main>
